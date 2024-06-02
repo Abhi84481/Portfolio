8832665caacd23ad718ae2f7a84fa9f3accf7e25
Abhi84481 <abhishekkurmatkar29@gmail.com>
2024-06-02 10:16:40 +0530

    made some changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Abhishek Kurmatkar.</h2>;
+  const three = <h3 className="big-heading">I am a DevOps Professional.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a DevOps engineer with experience in architecture and mission critical deployments over
+        large infrastructure. Proficient with configuration management tools and in developing CI/CD
+        pipeline.{' '}
+        <a href="https://www.mphasis.com/" target="_blank" rel="noreferrer">
+          Mphasis ltd
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
